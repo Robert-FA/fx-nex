ca95c44b0549150bce953aa7de45723668fd9af2
Robert Andersson <zinkcoin3@gmail.com>
2022-10-26 09:28:07 +0000

    added topic links

--- fx-nex-page/src/App.js
+++ fx-nex-page/src/App.js
@@ -13,16 +13,62 @@ function App() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Sounds</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Clips
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Connect</NavDropdown.Item>
+            <NavDropdown title="Travel" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Travel Trips</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Flights</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Transportations</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Hotels</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item href="#action/3.5">
+                Last Minute Bookings
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Weather" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Globel</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Forcast</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Live Report</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.5">
+                Latest 
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Rentals" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Holiday Apartments</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Long Term Rentals</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">T</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">H</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.5">
+                Last Minute Bookings
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="News" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Media</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Trends</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">T</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">H</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.5">
+                Last Minute Bookings
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Sports" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Events</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Latest & Upcoming</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Live Casts</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Betting</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.5">
+                Latest News
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Finance" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Stockmarkets</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Crypto</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Trends</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.5">
+                Latest 
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
